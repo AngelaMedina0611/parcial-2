c6df7b8bce31da58f57346684571a2b7d5d28ded
Angela Medina <angelamedina03.2016@gmail.com>
2026-04-23 22:50:55 -0500

    estructura jerarquica del arbol

--- arboles.py
+++ arboles.py
@@ -8,4 +8,8 @@ class Nodo:
         # Método para añadir un hijo (subcarpeta o archivo) al nodo actual
         self.hijos.append(hijo)
 
-    
+    def mostrar(self, nivel=1): # 
+        #Muestra la estructura jerárquica del árbol con indentación
+        print("   " * (nivel - 1) + f"Nivel {nivel}: {self.nombre}")
+        for hijo in self.hijos:
+            hijo.mostrar(nivel + 1)   # Llamada recursiva para mostrar los hijos

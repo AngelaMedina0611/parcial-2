ecddcbbf8a070b318487f4c324816bca1a617769
Angela Medina <angelamedina03.2016@gmail.com>
2026-04-23 22:48:59 -0500

    añadir un hijo a la subcarpeta

--- arboles.py
+++ arboles.py
@@ -3,3 +3,9 @@ class Nodo:
     def __init__(self, nombre):
         self.nombre = nombre      # Nombre del nodo (ej. "Documentos", "informe.txt")
         self.hijos = []           # Lista de hijos (subcarpetas o archivos dentro)
+    
+    def agregar_hijo(self, hijo):
+        # Método para añadir un hijo (subcarpeta o archivo) al nodo actual
+        self.hijos.append(hijo)
+
+    
